dcc962708c9943d78e7326842f38f884d59a62fd
Kwon_SangHeon <sangheon0187@gmail.com>
2026-03-11 17:46:52 +0900

    0311

---
+++ src/log_design.py
@@ -0,0 +1,10 @@
+# log_definition_sample
+user_event = {
+    "event_name": "view_remodeling_estimate",
+    "parameters": {
+        "apartment_id": "A1234",
+        "construction_year": 2005, 
+        "user_segment": "new_buyer", 
+        "predicted_demand_type": "full_remodeling"
+    }
+}
